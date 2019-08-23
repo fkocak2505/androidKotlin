d9b5d7194639c9ea1420016ffc3c33303dc23e23
Fatih Koçak <fatih.kocak@onedio.com>
2019-08-23 10:32:30 +0300

    update
    update

--- ByTranslator/app/src/main/java/tr/com/fkocak/bytranslator/api/interfaced/ICommandCallService.java
+++
@@ -1,8 +0,0 @@
-package tr.com.fkocak.bytranslator.api.interfaced;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-
-public interface ICommandCallService {
-    public <T> void callAsync(Call<T> tCall, Callback<T> callback);
-}
--- ByTranslator/app/src/main/java/tr/com/fkocak/bytranslator/tree/interfaced/LayoutItemType.java
+++
@@ -1,9 +0,0 @@
-package tr.com.fkocak.bytranslator.tree.interfaced;
-
-/**
- * Created by fatihkocak on 17.10.2018.
- */
-
-public interface LayoutItemType {
-    int getLayoutId();
-}
